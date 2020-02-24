4d9851b45ab43fdb6936216be01f8ab72fb6c054
Jennifer(Lakshmi) Miller <jmillerarcher@me.com>
2020-02-24 02:17:53 +0000

    Automatically backed up by Learn

--- spec/css_spec.rb
+++ spec/css_spec.rb
@@ -1,22 +1,22 @@
-require "pry"
+# require "pry"
 
-RSpec.describe 'styling' do
+# RSpec.describe 'styling' do
 
-  context 'within index.html' do
+#   context 'within index.html' do
 
-    it 'contains a <link> tag' do
-      link = parsed_html.search('link')[0]
-      expect(link).to_not be_nil, "No <link> tag was found"
-    end
-  end
+#     it 'contains a <link> tag' do
+#       link = parsed_html.search('link')[0]
+#       expect(link).to_not be_nil, "No <link> tag was found"
+#     end
+#   end
 
 
-  context 'within style.css' do
-    it 'looks like the finished product' do
-      # set this to true once your image looks like the solution displayed in the readme!
-      my_css_looks_like_the_solution_image = true
-      expect(my_css_looks_like_the_solution_image).to be_truthy
-    end
-  end
+#   context 'within style.css' do
+#     it 'looks like the finished product' do
+#       # set this to true once your image looks like the solution displayed in the readme!
+#       my_css_looks_like_the_solution_image = true
+#       expect(my_css_looks_like_the_solution_image).to be_truthy
+#     end
+#   end
 
-end
+# end
